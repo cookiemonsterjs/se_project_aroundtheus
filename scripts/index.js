012cd2d0ab0f1e7d7e7f60bc711017c32a497688
Julie <julie.shamir@gmail.com>
2025-01-31 07:37:20 +0200

    made corretions based on review

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,57 @@
 const initialCards = [
-    {
-      name: "Yosemite Valley",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-    },
-    {
-      name: "Lake Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-    },
-    {
-      name: "Bald Mountains",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-    },
-    {
-      name: "Vanoise National Park",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-    },
-  ];
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    alt: "Yosemite Valley",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    alt: "Lake Louise",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    alt: "Bald Mountains",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    alt: "Latemar",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    alt: "Vanoise National Park",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    alt: "Lago di Braies",
+  },
+];
 
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileTitleInput = document.querySelector("#profile-title");
+const profileSubtitleInput = document.querySelector("#profile-subtitle");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#cards-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
-  const profileEditButton = document.querySelector("#profile-edit-button");
-  const profileEditModal = document.querySelector("#profile-edit-modal");
-  const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
-  const profileTitle = document.querySelector(".profile__title");
-  const profileSubtitle = document.querySelector(".profile__subtitle"); 
-  const profileTitleInput = document.querySelector("#profile-title");
-  const profileSubtitleInput = document.querySelector("#profile-subtitle"); 
-  const profileEditForm = profileEditModal.querySelector(".modal__form");
-  const cardTemplate = document.querySelector("#cards-template").content.firstElementChild;
-  const cardListEl = document.querySelector(".cards__list");
-
-
-  function closePopup() {
-    profileEditModal.classList.remove("modal_opened");
-  }
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
 
 function handleProfileEditSubmit(e) {
   e.preventDefault();
-  profileTitle.textContent= profileTitleInput.value;
+  profileTitle.textContent = profileTitleInput.value;
   profileSubtitle.textContent = profileSubtitleInput.value;
-  closePopup()
-
+  closePopup();
 }
 
 function getCardElement(cardData) {
@@ -55,32 +59,23 @@ function getCardElement(cardData) {
   const cardImageEl = cardElement.querySelector(".cards__image");
   const cardTitleEl = cardElement.querySelector(".cards__name");
   cardTitleEl.textContent = cardData.name;
-  cardImageEl.src =cardData.link;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.alt;
   return cardElement;
 }
 
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileSubtitleInput.value = profileSubtitle.textContent;
 
+  profileEditModal.classList.add("modal_opened");
+});
 
-  profileEditButton.addEventListener("click", () => {
-    profileTitleInput.value = profileTitle.textContent;
-    profileSubtitleInput.value= profileSubtitle.textContent; 
-    
-  profileEditModal.classList.add("modal_opened"); 
-  });
-  
-
-  profileEditCloseButton.addEventListener("click", closePopup);
-  
-profileEditForm.addEventListener("submit", (e) => {
-
-})
- 
+profileEditCloseButton.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
- 
-initialCards.forEach((cardData)=> {
+
+initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
   cardListEl.append(cardElement);
-
-
-})
+});
